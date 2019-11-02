47a4f95ca4d54c83ba6168f3fa16a10e7c177c2e
Sören Johanson <so.johanson@outlook.com>
2019-11-02 13:49:09 +0100

    add file list, removal

--- index.php
+++ index.php
@@ -9,38 +9,38 @@
         <link rel="stylesheet" href="./public/css/bootstrap.min.css">
         <link rel="stylesheet" href="./public/css/main.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
-        <script src="../public/js/main.js" integrity="sha384-HvaLN1YH7wB0wInkz7frD1BNHlBjf+FhL1JsGq5+X+zT2iaTxPncpZ9NUF1PimfC" crossorigin="anonymous"></script>
+        <script src="../public/js/main.js"></script>
     </head>
 
     <body>
         <div class="h-100 row align-items-center">
             <div class="col d-flex justify-content-center">
-                <form enctype="multipart/form-data" action="src/upload.php" method="POST" class="needs-validation" novalidate>
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="customFile" name="file[]" multiple required>
-                        <label class="custom-file-label" for="customFile">Choose file(s)</label>
-                        <div class="invalid-feedback">
-                            Please select a valid file.
-                        </div>
-                    </div><br/><br/>
+                <form enctype="multipart/form-data" class="col-sm-3" action="src/upload.php" method="POST" class="needs-validation" novalidate>
                     <div class="form-group">
-                    <select class="custom-select" name="duration" required>
-                        <option value="">Duration</option>
-                        <option value="24">24h</option>
-                    </select>
-                    <div class="invalid-feedback">Please select a duration.</div>
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="customFile" name="file[]" multiple required>
+                            <label class="custom-file-label" for="customFile" id="custom-file-lbl">Choose file(s)</label>
+                            <div class="invalid-feedback">
+                                Please select a valid file.
+                            </div>
+                        </div>
+                        <ul class="list-group d-none" id="custom-file-list">
+                        </ul>
+                    </div>
+                    <div class="form-group">
+                        <select class="custom-select" name="duration" required>
+                            <option value="">Duration</option>
+                            <option value="8">8h</option>
+                            <option value="16">16h</option>
+                            <option value="24">24h (+2€)</option>
+                            <option value="48">48h (+5€)</option>
+                        </select>
+                        <div class="invalid-feedback">Please select a duration.</div>
                     </div>
                     <button type="submit" class="btn btn-primary">Upload</button>
                     <a class="btn btn-secondary" href="src/download.php" role="button">Download file</a>
                 </form>
             </div>
         </div>
-
-        <script>
-            $(document).ready(function () {
-                bsCustomFileInput.init()
-            });
-        </script>
     </body>
 </html>
--- public/js/main.js
+++ public/js/main.js
@@ -13,5 +13,53 @@
             form.classList.add('was-validated');
         }, false);
         });
+
+
+        document.getElementById('customFile').addEventListener('change', function (e) {
+            let fileList = document.getElementById("custom-file-list");
+            Array.from(e.target.files).forEach(file => {
+                let size = humanFileSize(file.size, false);
+                let name = file.name;
+
+                let fileElement = document.createElement("li");
+                let removeLink = document.createElement("a");
+
+                fileElement.setAttribute("id", "file-" + Math.floor((Math.random() * 250) + 1))
+
+                removeLink.setAttribute("href", "#");
+                removeLink.setAttribute("data-element", fileElement.id);
+                removeLink.classList.add("badge", "badge-danger");
+                removeLink.appendChild(document.createTextNode("X"));
+
+
+                removeLink.addEventListener("click",function(e) {
+                   document.getElementById(e.target.dataset.element).remove();
+                });
+
+                fileElement.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+                fileElement.appendChild(document.createTextNode(size + ": " + name));
+                fileElement.appendChild(removeLink);
+
+                fileList.appendChild(fileElement);
+
+            });
+            fileList.classList.remove("d-none");
+        });
     }, false);
+
+    function humanFileSize(bytes, si) {
+        var thresh = si ? 1000 : 1024;
+        if(Math.abs(bytes) < thresh) {
+            return bytes + ' B';
+        }
+        var units = si
+            ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+            : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+        var u = -1;
+        do {
+            bytes /= thresh;
+            ++u;
+        } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+        return bytes.toFixed(1)+' '+units[u];
+    }
 })();
